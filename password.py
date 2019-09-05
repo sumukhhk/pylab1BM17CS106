f20eb09ba05e14add78ea2b539f27cc634e9a3e2
sumukhhk <51911251+sumukhhk@users.noreply.github.com>
2019-09-05 12:57:12 +0530

    Update password.py

--- password.py
+++ password.py
@@ -4,7 +4,8 @@ s1=random.choice(string.ascii_lowercase)
 s2=random.choice(string.ascii_uppercase)
 s3=random.choice(string.punctuation)
 s4=random.choice(string.digits)
-letters=string.ascii_lowercase+string.ascii_lowercase+string.digits+string.punctuation
-password=s1+s2+s3+s4+''.join(random.sample(letters,8))
-p=''.join(random.sample(password,12))
+letters=string.ascii_lowercase+string.ascii_uppercase+string.digits+string.punctuation
+len=random.randint(4,16)
+password=s1+s2+s3+s4+''.join(random.sample(letters,l))
+p=''.join(random.sample(password,l+4))
 print(p)
